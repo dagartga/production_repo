6d7bb96dd776e567bde49c7eee9e9f5227dfd97d
Dagart Allison <dagartga@gmail.com>
2021-12-10 23:43:58 +0000

    add url + features

--- btcinfocharts_scraper.py
+++ btcinfocharts_scraper.py
@@ -6,8 +6,30 @@ def current_date():
     today = date.today()
     return today
     
+print(current_date())
     
-
-
-
+def scrape_data():
+    
+    # the list of features to scrape
+    features = ['median_transaction_fee3momUSD',
+                'fee_to_reward7momUSD',
+                'top100cap30trx',
+                'mining_profitability7rsi',
+                'top100cap14mom',
+                'price3wmaUSD',
+                'price30smaUSD',
+                'price30emaUSD',
+                'hashrate30sma',
+                'difficulty30sma',
+                'price3rsiUSD',
+                'confirmationtime7std']
+    
+    url_list = [] # create empty list for urls
+    
+    for feat in features:
+        url='https://bitinfocharts.com/comparison/'+feat
+        url_list.append(url)
+    
+    assert 'https://bitinfocharts.com/comparison/median_transaction_fee3momUSD' == url_list[0]
+    assert 'https://bitinfocharts.com/comparison/confirmationtime7std' == url_list[-1]
     
