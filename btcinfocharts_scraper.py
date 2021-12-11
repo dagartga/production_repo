368a248bc44c1a808a5cdcd0cdd9be8a61bd72c4
Dagart Allison <dagartga@gmail.com>
2021-12-11 03:38:15 +0000

    add feats_to_url function

--- btcinfocharts_scraper.py
+++ btcinfocharts_scraper.py
@@ -10,3 +10,47 @@ def current_date():
     # get the current date
     today = date.today()
     return str(today).replace('-', '/')
+    
+
+
+
+
+def feats_to_url():
+    """
+        Takes a list of string features
+        and converts to a list of the urls
+    """
+    
+    # the list of features to scrape
+    features = ['median_transaction_fee3momUSD',
+                'fee_to_reward7momUSD',
+                'top100cap30trx',
+                'mining_profitability7rsi',
+                'top100cap14mom',
+                'price3wmaUSD',
+                'price30smaUSD',
+                'price30emaUSD',
+                'hashrate30sma',
+                'difficulty30sma',
+                'price3rsiUSD',
+                'confirmationtime7std']
+    
+    import re
+    
+    url_list = []
+    
+    for feat in features:
+   
+        regex = re.compile(r'(top100cap|[a-z_]+)(\d+\w+)')
+        re_result = regex.search(feat)
+        
+        feat = re_result.group(1)
+        trans = re_result.group(2)
+    
+        url='https://bitinfocharts.com/comparison/'+feat+'-'+'btc'+'-'+trans+'.html'
+        url_list.append(url)
+        
+ 
+    return url_list, features
+    
+    
