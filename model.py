8ae821c42ad7d3c005f37a3e2da447582cba891e
Dagart Allison <dagartga@gmail.com>
2021-12-14 01:50:14 +0000

    add imports and load_model

---
+++ model.py
@@ -0,0 +1,13 @@
+def model():
+    
+    """
+        Takes in the current data scraped from btcinfocharts.org
+        and returns a price prediction for tomorrow
+    """
+    
+    from keras.models import load_model
+    import h5py
+    
+    # load the best model from the training and testing
+    model = load_model('ANN4_reg_nextday300Adam0-01relu64Int4_341.hdf5')
+    
