71894508faf97ade2e236c123298e81a0d8ed932
Dagart Allison <dagartga@gmail.com>
2021-12-18 10:34:06 -0800

    add Date column to get_full_dataset function

--- model.py
+++ model.py
@@ -3,12 +3,12 @@ def get_full_dataset():
         Load in the full dataset scraped from btcinfocharts
         Extract and return only a dataframe with the selected features
     """
-       
+
     import pandas as pd
 
     path = './Merged_Unconverted_BTC_Data.csv'
     full_df = pd.read_csv(path)
-    
+
     features = [
             'median_transaction_fee3momUSD',
             'fee_to_reward7momUSD',
@@ -20,10 +20,13 @@ def get_full_dataset():
             'difficulty30sma',
             'fee_to_reward90smaUSD'
             ]
-    
+
+    # combine date and features
+    columns = ['Date'] + features
+
     # extract only the data from the features desired
-    select_df = full_df[features]
-    
+    select_df = full_df[columns]
+
     return select_df
 
 
@@ -33,63 +36,62 @@ def preprocess_the_data():
         to fit the scaler using MinMaxScaler and RobustScaler
         Then transform the new data using the scaler and retun it as a vector
     """
-    
+
     from sklearn.preprocessing import MinMaxScaler, RobustScaler
     from sklearn.pipeline import Pipeline
     from btcinfocharts_scraper import grab_the_data
-  
+
     # scale the data
     estimators = [] # create a list for the scalers
     estimators.append(['minmax', MinMaxScaler()])
     estimators.append(['robust', RobustScaler()])
-    
+
     # add the scalers to the Pipeline
     scale = Pipeline(estimators, verbose=True)
-    
+
     # get the full dataset
     df = get_full_dataset()
-    
+
     # extract the data from the dates of Interval 4
     date_bool = (df.iloc[:, 0] >= '2013/04/01') & (df.iloc[:, 0] <= '2021/09/01')
     df = df[date_bool]
     # drop the date column
     df = df.drop(columns=['Date'])
-    
+
     # get the new dataset for the most recent data
     new_df = grab_the_data()
-    
+
     # fit the scaler to all old data
     scale.fit(df)
-    
+
     # scale the new data
     transformed_new_data = scale.transform(new_df)
-    
+
     return transformed_new_data
-     
-    
+
+
 def prediction():
-    
+
     """
         Takes in the current data scraped from btcinfocharts.org
         and returns a price prediction for tomorrow
         Output is a 2D array
         Example array([[59300.715]], dtype=float32)
     """
-    
+
     from keras.models import load_model
-    
+
     # load the best model from the training and testing
     ann_model = load_model('./trained_models/ANN4_reg_nextday300Adam0-01relu64Int4_341.hdf5')
-    
+
     # load the current scaled data
     current_scaled_data = preprocess_the_data()
-    
+
     # make the prediction
     pred_next_day_price = ann_model.predict(current_scaled_data)
-    
-    return pred_next_day_price
-    
-    
 
-    
-    
+    return pred_next_day_price
+
+
+
+prediction()
