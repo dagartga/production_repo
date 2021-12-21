90344f503d19a2141596dd3f00e9efd21dfd2aac
Dagart <65997824+dagartga@users.noreply.github.com>
2021-12-20 16:20:21 -0800

    add fully passing scraper test

--- test_btcinfocharts_scraper.py
+++ test_btcinfocharts_scraper.py
@@ -35,7 +35,6 @@ def test_features():
     assert 'https://bitinfocharts.com/comparison/median_transaction_fee-btc-3momUSD.html' in url_list
     assert 'https://bitinfocharts.com/comparison/top100cap-btc-14mom.html' in url_list
     assert 'https://bitinfocharts.com/comparison/fee_to_reward-btc-90smaUSD.html' in url_list
-    assert 'blah' in url_list # should fail
 
     # test all the features were turned into
     assert len(url_list) == len(feature_list)
@@ -45,13 +44,20 @@ def test_create_dfs():
 
     import pandas as pd
 
-    url_list = ['https://bitinfocharts.com/comparison/median_transaction_fee-btc-3momUSD.html',, 'https://bitinfocharts.com/comparison/top100cap-btc-14mom.html']
+    url_list = ['https://bitinfocharts.com/comparison/median_transaction_fee-btc-3momUSD.html', 'https://bitinfocharts.com/comparison/top100cap-btc-14mom.html']
     feat_list = ['median_transaction_fee3momUSD', 'top100cap14mom']
 
-    df_url, df_feats = create_dfs(url_list, feat_list)
+    urls, feats = feats_to_url()
 
-    assert df_url == pd.DataFrame(url_list)
-    assert df_feats == pd.DataFrame(feat_list)
+    df_url, df_feats = create_dfs(urls, feats)
+
+    assert len(df_url) == len(df_feats)
+
+    assert df_url.iloc[0,0] == url_list[0]
+    assert df_url.iloc[4,0] == url_list[1]
+    assert df_feats.iloc[0,0] == feat_list[0]
+    assert df_feats.iloc[4,0] == feat_list[1]
+
 
 
 def test_scrape_url():
@@ -59,22 +65,21 @@ def test_scrape_url():
     from btcinfocharts_scraper import scrape_url
     from datetime import date, timedelta
 
-    json_scrape = scrape_url()
+    url='https://bitinfocharts.com/comparison/median_transaction_fee-btc-3momUSD.html'
+    feature='median_transaction_fee3momUSD'
+
+    json_scrape = scrape_url(url, feature)
 
     # check that function scraped two values
     assert len(json_scrape) == 2
 
-    # check that the proper date is in the proper location
-    assert tday == json_scrape[0]
-    assert yday == json_scrape[1]
+    # check that the proper dates are collected
+    assert tday in json_scrape
+    assert yday in json_scrape
 
-    # check that all the features were stored
-    assert len(json_scrape[tday]) == len(features)
-    assert len(json_scrape[yday]) == len(features)
-
-    for feat in features:
-        assert feat in json_scrape[tday]
-        assert feat in json_scrape[yday]
+    # check that the feature is a value for each day
+    assert feature in json_scrape[tday]
+    assert feature in json_scrape[yday]
 
 
 def test_grab_the_data():
@@ -82,14 +87,7 @@ def test_grab_the_data():
     df = grab_the_data()
 
     # check that all the features are in the columns
-    assert features == df.columns
+    for i in range(len(features)):
+        assert features[i] == df.columns[i]
     # check that the date is either today or yesterday
-    assert df.index[0] == yday
-    assert df.index[1] == tday
-
-
-print('Testing')
-test_features()
-print('Done')
-
-# virtual env ~/.btc-test/Scripts/activate
+    assert df.index[0] == tday or df.index[0] == yday
