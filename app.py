d1c14d89eff672eff1faa20191dfa2ca386aa909
Dagart <65997824+dagartga@users.noreply.github.com>
2022-01-19 16:35:16 -0800

    change to port 3000

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def home():
     return "Welcome to the BTC Next Day Price Predictor"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='8080')
+    app.run(host='0.0.0.0', port='3000')
